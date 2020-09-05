f9e37c9376fc768dcab4d4db7d4c934c0c7e4e76
werfxz <furkan.cetin1@yandex.com>
2020-09-05 20:59:49 +0300

    seller ids inserted while creating database

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
-import sqlite3
 import os
+import time
+import sqlite3
 
 class Database:
 
@@ -7,10 +8,22 @@ class Database:
         #connect db if it exist 
         if os.path.isfile('products.db'):
             self.conn = sqlite3.connect('products.db')
-        #if db doesn't exist then first create db then tables
+        #if db doesn't exist first create db then tables
         else:
+            #crete db
             self.conn = sqlite3.connect('products.db')
+            #create tables
             self.create_tables()
+
+        #inser seller ids to database
+        self.seller_ids = [(1, 'hepsiburada'),
+                           (2, 'trendyol'),
+                           (3, 'amazon'),
+                           (4, 'vatan'),
+                           (5, 'teknosa'),
+                           (6, 'incehesap'),
+                           (7, 'itopya')]
+        self.inser_sellers()
 
     def create_tables(self):
         """
@@ -30,8 +43,32 @@ class Database:
         #Price of the product table
         c.execute('''CREATE TABLE products_prices
              (date text, product_id integer, seller_id integer, price real)''')
+    
+        c.executemany('INSERT INTO sellers VALUES (?,?)', self.seller_ids)
 
         self.conn.commit()
         self.conn.close()
-    
-    
+
+    def inser_sellers(self):
+        """
+        This functions inserts sellers id at the db creation
+        """
+        c = self.conn.cursor()
+
+        c.executemany('INSERT INTO sellers VALUES (?,?)', self.seller_ids)
+
+        self.conn.commit()
+        self.conn.close()
+
+    def insert_price(self, product):
+        """
+        This functions inserts prices of the product scraped from different sellers
+        """
+        c = self.conn.cursor()
+
+        c.executemany('INSERT INTO product_prices VALUES (?,?,?,?)', ('date, product_id, seller_id, price'))
+
+        self.conn.commit()
+        self.conn.close()
+
+        pass
